6c0eb063b25a3e8c39bcf2cfb79b9db4e4e4125b
Hrithik Keswani <hrithikkeswani200@gmail.com>
2023-05-25 09:56:55 +0530

    cal test file
    updated calc test file

--- tests/mycalculatordapp.test.js
+++ tests/mycalculatordapp.test.js
@@ -1,27 +1,30 @@
-const assert=require("assert");
-const anchor= require("@project-serum/anchor");
-const {SystemProgram}=anchor.web3 
+const assert = require("assert");
+const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
 
-const hell=("mycalculatordapp",async ()=>{
-    const provider=anchor.AnchorProvider.env();
-     anchor.setProvider(provider); 
-     const calculator =anchor.web3.Keypair.generate();//escrow
-     const program= anchor.workspace.Mycalculatordapp;//idl
+const hell =
+  ("mycalculatordapp",
+  async () => {
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
+    const calculator = anchor.web3.Keypair.generate(); //escrow
+    const program = anchor.workspace.Mycalculatordapp; //idl
 
-const tx= await program.rpc.create("Welcome to Solana",
-        {
-            accounts:{
-                calculator:calculator.publicKey,//new keypair //verification 11 //12
-                user:provider.wallet.publicKey,//phantom //cli mukesh 
-                systemProgram:SystemProgram.programId //verifcation
-            },
-            signers:[calculator]  //12
-        })
+    const tx = await program.rpc.create("Welcome to Solana", {
+      accounts: {
+        calculator: calculator.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [calculator],
+    });
 
-        const account= await program.account.calculator.fetch(calculator.publicKey)  //12
-        console.log("account",account)
-        assert.ok(account.greeting=="Welcome to Solana")
-     
-        console.log("Transaction",tx)
-    })
-    hell()
+    const account = await program.account.calculator.fetch(
+      calculator.publicKey
+    );
+    console.log("account", account);
+    assert.ok(account.greeting == "Welcome to Solana");
+
+    console.log("Transaction", tx);
+  });
+hell();
